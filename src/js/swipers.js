b1504054dc3a077a5822e2fbd903a06768403f33
Gleb Leus <lv.gleb44@gmail.com>
2023-12-05 18:43:20 +0300

    buildings page

--- src/js/swipers.js
+++ src/js/swipers.js
@@ -87,3 +87,77 @@ const exampleSwiper = new Swiper(".example__swiper", {
       },
    },
 });
+
+const requirementsSwiper = new Swiper(".requirements__swiper", {
+   slidesPerView: 1,
+   slidesPerGroup: 1,
+
+   navigation: {
+      nextEl: ".requirements__swiper-next",
+      prevEl: ".requirements__swiper-prev",
+   },
+});
+
+const examplesSwiper = new Swiper(".examples__swiper", {
+   slidesPerView: 1.75,
+   slidesPerGroup: 1,
+   spaceBetween: 10,
+
+   breakpoints: {
+      768: {
+         slidesPerView: 3.9,
+         slidesPerGroup: 1,
+         spaceBetween: 20,
+      },
+   },
+
+   on: {
+      init: function (swiper) {
+         const slides = this.slides;
+
+         if (window.screen.width > 768) {
+            const activeSlide = swiper.activeIndex + 1;
+
+            slides[activeSlide].classList.add("show-name");
+
+            slides.forEach((slide, i) => {
+               if (i == activeSlide) {
+                  return;
+               }
+
+               if (i % 2 !== 0) {
+                  slide.classList.add("bottom-active");
+               }
+            });
+         } else {
+            const activeSlide = swiper.activeIndex;
+
+            slides[activeSlide].classList.add("show-name");
+         }
+      },
+
+      slideChange: function (swiper) {
+         let slides = this.slides;
+
+         if (window.screen.width > 768) {
+            let activeSlide = swiper.activeIndex + 1;
+
+            slides[activeSlide - 1].classList.remove("show-name");
+            slides[activeSlide + 1].classList.remove("show-name");
+            slides[activeSlide].classList.add("show-name");
+
+            slides[activeSlide + 1].classList.remove("bottom-active");
+            slides[activeSlide + 2].classList.add("bottom-active");
+         } else {
+            let activeSlide = swiper.activeIndex;
+
+            if (activeSlide !== 0) {
+               slides[activeSlide - 1].classList.remove("show-name");
+            }
+
+            slides[activeSlide + 1].classList.remove("show-name");
+            slides[activeSlide].classList.add("show-name");
+         }
+      },
+   },
+});
